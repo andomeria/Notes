a8033a93ed93addfe3bba1cc810c98f547fcd4e6
lisse <lisse@LOTTE>
2022-08-11 10:43:09 +0200

    Add project files.

---
+++ Notes/Notes/Views/NoteEntryPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Notes.Models;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public string ItemId
+        {
+            set
+            {
+                LoadNote(value);
+            }
+        }
+
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+
+            // Set the BindingContext of the page to a new Note.
+            BindingContext = new Note();
+        }
+
+        void LoadNote(string filename)
+        {
+            try
+            {
+                // Retrieves the note and sets it as the BindingContext of the page.
+                Note note = new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                };
+                BindingContext = note;
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load note.");
+            }
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // Saves the file.
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Updates the file.
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            // Navigates backwards
+            await Shell.Current.GoToAsync("..");
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            // Deletes the file.
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+
+            // Navigates backwards
+            await Shell.Current.GoToAsync("..");
+        }
+
+    }
+}
---
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Notes.Models;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+
+            // Defines that it should create a Note object from each file.
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach (var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+            // Sets the data source for the CollectionView to a sorted collection of notes.
+            collectionView.ItemsSource = notes
+                .OrderBy(d => d.Date)
+                .ToList();
+        }
+
+        async void OnAddClicked(object sender, EventArgs e)
+        {
+            // Navigates to the NoteEntryPage, without passing any data.
+            await Shell.Current.GoToAsync(nameof(NoteEntryPage));
+        }
+
+        async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                // Navigates to the NoteEntryPage, passing the filename as a query parameter.
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.Filename}");
+            }
+        }
+    }
+}
